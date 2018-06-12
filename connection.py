4035512cdc42113b06b3855412c0bc699927b7c9
Felipe Ferreira <felipe.gomes.ferreira@gmail.com>
2018-06-12 20:54:01 -0300

    comeco do form de configuracao de conexoes

---
+++ connection.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'connection.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_connection(object):
+    def setupUi(self, connection):
+        connection.setObjectName("connection")
+        connection.resize(193, 236)
+        connection.setModal(True)
+        self.verticalLayout = QtWidgets.QVBoxLayout(connection)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.name = QtWidgets.QLineEdit(connection)
+        self.name.setObjectName("name")
+        self.verticalLayout.addWidget(self.name)
+        self.host = QtWidgets.QLineEdit(connection)
+        self.host.setObjectName("host")
+        self.verticalLayout.addWidget(self.host)
+        self.database = QtWidgets.QLineEdit(connection)
+        self.database.setObjectName("database")
+        self.verticalLayout.addWidget(self.database)
+        self.username = QtWidgets.QLineEdit(connection)
+        self.username.setObjectName("username")
+        self.verticalLayout.addWidget(self.username)
+        self.password = QtWidgets.QLineEdit(connection)
+        self.password.setObjectName("password")
+        self.verticalLayout.addWidget(self.password)
+        self.port = QtWidgets.QSpinBox(connection)
+        self.port.setMinimum(1)
+        self.port.setMaximum(65000)
+        self.port.setProperty("value", 3306)
+        self.port.setObjectName("port")
+        self.verticalLayout.addWidget(self.port)
+        self.buttonBox = QtWidgets.QDialogButtonBox(connection)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        self.verticalLayout.addWidget(self.buttonBox)
+
+        self.retranslateUi(connection)
+        self.buttonBox.rejected.connect(connection.close)
+        QtCore.QMetaObject.connectSlotsByName(connection)
+
+    def retranslateUi(self, connection):
+        _translate = QtCore.QCoreApplication.translate
+        connection.setWindowTitle(_translate("connection", "Conexão"))
+        self.name.setPlaceholderText(_translate("connection", "Nome da Conexão"))
+        self.host.setPlaceholderText(_translate("connection", "Host"))
+        self.database.setPlaceholderText(_translate("connection", "Base de Dados"))
+        self.username.setPlaceholderText(_translate("connection", "Usuário"))
+        self.password.setPlaceholderText(_translate("connection", "Senha"))
+        self.port.setToolTip(_translate("connection", "Porta TCP"))
+
--- kquery.py
+++ kquery.py
@@ -1,18 +1,21 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtGui import QStandardItem, QStandardItemModel, QSett
+from PyQt5.QtGui import QStandardItem, QStandardItemModel
 from PyQt5.QtCore import QSettings
 import mainwindow
 import connections
+import connection
 import psycopg2
 
 
-connection = None
+connection_state = None
 _LIST_TABLES = ("select relname "
                 "from pg_class "
                 "where relkind='r' and "
                 "relname !~ '^(pg_|sql_)' "
                 "order by relname asc")
+
+_CONNECTIONS = "connections"
 
 
 def listtables():
@@ -27,12 +30,57 @@ def splitquerybyposition(query, position):
     return querysliced[0].split(";")[-1] + querysliced[1].split(';')[0]
 
 
+class Connection(QtWidgets.QDialog):
+    def __init__(self, parent):
+        super(Connection, self).__init__(parent=parent)
+        self.ui = connection.Ui_connection()
+        self.ui.setupUi(self)
+        self.ui.buttonBox.accepted.connect(self.accepted)
+    
+    def accepted(self):
+        ui = self.ui
+        parameters = {
+            "name": ui.name.text(),
+            "host": ui.host.text(),
+            "database": ui.database.text(),
+            "username": ui.username.text(),
+            "password": ui.password.text(),
+            "port": ui.port.value()
+        }
+
+        connections = settings.value(_CONNECTIONS)
+        if not connections:
+            connections = []
+        connections += [parameters]
+
+        settings.setValue(_CONNECTIONS, connections)
+        self.close()
+        self.parent().read_connection_settings()
+
+
 class Connections(QtWidgets.QDialog):
     def __init__(self, parent):
         super(Connections, self).__init__(parent=parent)
         self.ui = connections.Ui_connections()
         self.ui.setupUi(self)
-
+        self.read_connection_settings()
+        self.ui.add.clicked.connect(self.new_connection)
+    
+    def new_connection(self):
+        new_connection = Connection(self)
+        new_connection.show()
+    
+    def read_connection_settings(self):
+        settings = QSettings(parent=self)
+        stored_connections = settings.value(_CONNECTIONS)
+        if stored_connections:
+            model = QStandardItemModel()
+            for nconnection, connection in enumerate(stored_connections):
+                import ipdb; ipdb.set_trace()
+                model.setItem(nconnection, 0, QStandardItem(connection['name']))
+                model.setItem(nconnection, 1, QStandardItem(connection['username']))
+                model.setItem(nconnection, 2, QStandardItem(connection['host']))
+            self.ui.connectionslist.setModel(model)
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -41,14 +89,14 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.ui.executequery.clicked.connect(self.execute_query)
         self.ui.actionConex_es.triggered.connect(self.show_connections)
-        model = QStandardItemModel()
-        #for ntable, table in enumerate(listtables()):
-        #    model.setItem(ntable, 0, QStandardItem(str(table)))
+
+        # model = QStandardItemModel()
+        # for ntable, table in enumerate(listtables()):
+        #     model.setItem(ntable, 0, QStandardItem(str(table)))
         
-        self.ui.tables.setModel(model)
+        # self.ui.tables.setModel(model)
 
     def show_connections(self):
-        print("fui chamado")
         connections_window = Connections(self)
         connections_window.show()
 
@@ -70,9 +118,13 @@ class MainWindow(QtWidgets.QMainWindow):
   
             self.ui.tableresults.setModel(model)
 
-#connect()
 
 app = QtWidgets.QApplication(sys.argv)
+app.setOrganizationName("kquery")
+app.setOrganizationDomain("github.com/samambaman")
+
+settings = QSettings(app)
+settings.setValue(_CONNECTIONS, [])
 
 my_mainWindow = MainWindow()
 my_mainWindow.show()
